c7746e2688138a1fe902598caa99468bbb85bf1a
Harrison Nguyen <nguy2173@umn.edu>
2020-05-15 03:39:49 -0500

    link in activity 2 to html

--- JavaScript_Basics_Assessment/scripts/activity-2.js
+++ JavaScript_Basics_Assessment/scripts/activity-2.js
@@ -1,5 +1,11 @@
 // Grab a user input using a prompt() and store it in a variable.
 let userInput = prompt('Insert user input here');
 
-// Add a conditional statement where if the variable's length is greater than 4, we alert the user that their name is greater than four characters.
+// Add a conditional statement where if the variable's length is greater than 4, 
+// we alert the user that their name is greater than four characters.
 // Otherwise, alert that their name is less than four characters.
+if(userInput.length > 4){
+  alert('Your name is greater than 4 characters')
+} else {
+  alert('Your name is less than 4 characters')
+}
