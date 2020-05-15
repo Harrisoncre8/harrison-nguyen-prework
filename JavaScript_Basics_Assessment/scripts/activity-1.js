10466c134766e7cfb5169567fbfa31bac5904ce0
Harrison Nguyen <nguy2173@umn.edu>
2020-05-15 03:33:55 -0500

    link activity 1 to html

---
+++ JavaScript_Basics_Assessment/scripts/activity-1.js
@@ -0,0 +1,25 @@
+// the product of 9 and 9
+let product = 9*9;
+console.log(product); // 81
+
+// the quotient of 56 and 12
+let quotient = 56/12;
+console.log(quotient); // 4.666666666666667
+
+// The remainder of 281 divided by 9
+let remainder = 281%9;
+console.log(remainder); // 2
+
+// 18 divided by 0
+let answer = 18/0;
+console.log(answer); // infinity
+
+// The sum of string 56 and string 92
+let sum = "56" +"92";
+console.log(sum); // "5692"
+
+// Using any comparison operator you wish on the numbers 42 and 52, make it evaluate to false
+let untrue = 42 > 52;
+console.log(untrue); // false
+
+
---
+++ JavaScript_Basics_Assessment/scripts/activity-2.js
@@ -0,0 +1,5 @@
+// Grab a user input using a prompt() and store it in a variable.
+let userInput = prompt('Insert user input here');
+
+// Add a conditional statement where if the variable's length is greater than 4, we alert the user that their name is greater than four characters.
+// Otherwise, alert that their name is less than four characters.
