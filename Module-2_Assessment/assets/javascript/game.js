a7144c49150f5e3659b60739cbf254aee2a9898e
Harrison Nguyen <nguy2173@umn.edu>
2020-05-16 11:21:46 -0500

    organize game properties in object

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,13 @@
+// snowman game object
+let snowman = {
+  // list of words property 
+  wordList: ['igloo', 'snowflake', 'sled', 'scarf', 'mitten', 'hail', 'olaf', 'chilly'],
+  word: '',
+  lettersInWord: [],
+  renderWord: '',
+  lettersGuessed: [],
+  guessesRemaining: 0,
+  totalTries: 5,
+  wins: [],
+  continutePlay: true,
+}
