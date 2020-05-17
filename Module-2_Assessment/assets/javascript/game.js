1581bd662ebc23dbb42e0bf17b09c235be9b905c
Harrison Nguyen <nguy2173@umn.edu>
2020-05-16 23:31:28 -0500

    add method to handle user key press

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -6,42 +6,63 @@ let wordElement = document.querySelector('#currentWord');
 let snowmanGame = {
   // list of words property 
   wordList: ['igloo', 'snowflake', 'sled', 'scarf', 'mitten', 'hail', 'olaf', 'chilly'],
+  // the selected word is set here
   word: '',
-  lettersInWord: [],
+  // letter or underscore is set here
   renderWord: '',
+  // incorrect guesses
   lettersGuessed: [],
-  guessesRemaining: 0,
-  // amount of guesses the user has to solve the word
+  // whatever the user types in
+  userInput: '',
+  // set amount of guesses the user has to solve the word
   totalTries: 8,
+  // amount of tries the user has attempted 
+  triesRemaining: 0,
+  // amount of words the user has solve is set here
   wins: 0,
-  continutePlay: true,
+
   // selects random word from wordList and set it to word property
   selectWord: function(){
     this.word = this.wordList[ Math.floor(Math.random() * this.wordList.length) ];
   },
   // display underscores based on word length, 
-  // map through array of letters and display letter or underscore based on guess
-  setupRenderWord: function(){
+  // map through array of letters and use ternery operator
+  // to display letter or underscore based on guess
+  updateRenderWord: function(){
     this.renderWord = this.word.split('').map(
       letter => (this.lettersGuessed.indexOf(letter) >= 0 ? letter : ' _ ')).join('');
-
       wordElement.innerText = this.renderWord;
   },
-  //
+  handleUserInput: function(event){
+    let code = event.keyCode;
+    // validate that the user typed in a letter
+    if(code > 64 && code < 91 ){
+      // set string of key code to userInput
+      snowmanGame.userInput = String.fromCharCode(code);
+      // check to see if letter was already guessed
+      snowmanGame.lettersGuessed.indexOf(snowmanGame.userInput) === -1 ? snowmanGame.lettersGuessed.push(snowmanGame.userInput) : null;
+      
+      if(snowmanGame.word.indexOf(snowmanGame.userInput) >= 0){ 
+        snowmanGame.updateRenderWord();
+        // check if meet victory condition
+      }
+    }
+  },
   // method that starts the game
   playGame: function(){
     snowmanGame.selectWord();
-    snowmanGame.setupRenderWord();
-
-    // while loop running while playGame is called
-    // while( this.renderWord !== this.word){
-
-    // }
-  }
+    snowmanGame.updateRenderWord();
+  },
+  // method to start the snowman game and 
+  // removes event listener after one user key click
+  // user must refresh page to run the key click event listener 
+  startGame: function(){
+    snowmanGame.playGame();
+    gameElement.removeEventListener('keydown', snowmanGame.startGame);
+  },
 }
 
-function changeDOM (){
-  wordElement.innerText = snowmanGame.renderWord;
-}
+// on any key click, snowman game will start 
+gameElement.addEventListener('keydown', snowmanGame.startGame);      
 
-gameElement.addEventListener('keypress', snowmanGame.playGame);
+document.addEventListener('keydown', snowmanGame.handleUserInput);
